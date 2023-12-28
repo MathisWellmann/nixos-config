c365c99640c3483fc2ddb5de8fabbe919d57ea36
MathisWellmann <wellmannmathis@gmail.com>
2023-12-28 19:30:25 +0100

    Add  host

---
+++ hosts/superserver/configuration.nix
@@ -0,0 +1,141 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  # Enable ip forwarding for exposing tailscale subnet routes.
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  boot.kernel.sysctl."ipv6.conf.all.forwarding" = 1;
+
+  networking.hostName = "superserver"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "de";
+    xkbVariant = "";
+    videoDrivers = ["nvidia"];
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaSettings = true;
+  };
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.magewe = {
+    isNormalUser = true;
+    description = "magewe";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    helix
+    lsd
+    skim
+    ripgrep
+    zoxide
+    zellij
+    lfs
+    htop
+    bottom
+    neofetch
+    tree
+    nvtop
+    rustup
+    git
+    gitui
+    oxker
+    gcc
+    killall
+    iperf
+    lshw
+    nmap
+    rathole
+  ];
+
+  programs.bash.shellAliases = {
+    cu = "cargo update";
+    cc = "cargo check";
+    cb = "cargo build";
+    cbr = "cargo build --release";
+    cr = "cargo run";
+    crr = "cargo run --release";
+    cte = "cargo test";
+    fmt = "cargo +nightly fmt";
+    tfmt = "taplo fmt";
+  };
+
+  services.openssh.enable = true;
+  services.tailscale.enable = true;
+  services.prometheus = {
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+        port = 9002;
+      };
+    };
+  };
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
