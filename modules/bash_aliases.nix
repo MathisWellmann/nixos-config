fffc9ee93ab2f69f344a7b7a0b7ae5145cea3be1
MathisWellmann <wellmannmathis@gmail.com>
2024-01-12 20:18:44 +0100

    Update `bash_aliases.nix`

--- modules/bash_aliases.nix
+++ modules/bash_aliases.nix
@@ -1,6 +1,11 @@
 {...}: {
   programs.bash.shellAliases = {
+    ns = "nix-shell";
     la = "lsd -la";
+    skhx = "sk | xargs hx"; # Fuzzy search and open the selected file with `helix`
+    night = "redshift -P -O 5000";
+    bright = "sudo xbacklight -set 100";
+    # Rust
     cu = "cargo update";
     cc = "cargo check";
     cb = "cargo build";
@@ -10,5 +15,6 @@
     cte = "cargo test";
     fmt = "cargo +nightly fmt";
     tfmt = "taplo fmt";
+    udeps = "cargo +nightly udeps --all-targets";
   };
 }
