9fdee20f6cabde575deeb3dece8bc5f4d71f77a3
MathisWellmann <wellmannmathis@gmail.com>
2024-03-28 14:23:13 +0100

    Adds sound support

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -9,8 +9,19 @@
     nvidiaSettings = true;
     # package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+
+  # Sound
+  # Some tricks:
+  # systemctl --user restart pipewire.service
+  # systemctl --user restart pipewire-pulse.service
   sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
   services.xserver = {
     enable = true;
