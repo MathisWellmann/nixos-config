2e6d28472a4ead186c7fe324dde119e58f13f505
MathisWellmann <wellmannmathis@gmail.com>
2024-04-09 11:11:14 +0200

    Update `hyprland` monitors

--- home/home_hyprland.nix
+++ home/home_hyprland.nix
@@ -51,10 +51,10 @@
     # systemd.enable = true;
     settings = {
       monitor = [
-        "HDMI-A-1, preferred, 0x0, 1, transform, 1"
         "DP-1, preferred, 2160x0, 1, transform, 1"
-        "DP-3, preferred, 4320x0, 1, transform, 1"
-        "DP-2, preferred, 6480x0, 1"
+        "DP-2, preferred, 0x0, 1, transform, 1"
+        "DP-3, preferred, 6000x0, 1"
+        "HDMI-A-1, preferred, 4320x0, 1, transform, 1"
       ];
       "exec-once" = "waybar & hyprpaper";
       "$terminal" = "alacritty";
