fcc85ccd48adce5d83474a1fe9fdf51731d6f46d
MathisWellmann <wellmannmathis@gmail.com>
2024-06-11 19:47:46 +0200

    meshify: update monitors

--- home/meshify.nix
+++ home/meshify.nix
@@ -7,9 +7,8 @@
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [
-        "DP-1, preferred, 4320x0, 1, transform, 1"
-        "DP-2, preferred, 2160x0, 1, transform, 1"
         "DP-3, preferred, 0x0, 1, transform, 1"
+        "DP-2, preferred, 2160x0, 1, transform, 1"
       ];
     };
   };
