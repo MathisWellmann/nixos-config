116e202bd868ca75d5ac2bfc8bc9ac7c6dd3070c
MathisWellmann <wellmannmathis@gmail.com>
2024-07-10 20:28:46 +0200

    nfs mounts: add option `rw`

--- modules/mount_poweredge_exports.nix
+++ modules/mount_poweredge_exports.nix
@@ -3,13 +3,16 @@
   fileSystems."/mnt/poweredge_video" = {
     device = "poweredge:/SATA_SSD_POOL/video";
     fsType = "nfs";
+    options = ["rw"];
   };
   fileSystems."/mnt/poweredge_music" = {
     device = "poweredge:/SATA_SSD_POOL/music";
     fsType = "nfs";
+    options = ["rw"];
   };
   fileSystems."/mnt/poweredge_enc" = {
     device = "poweredge:/SATA_SSD_POOL/enc";
     fsType = "nfs";
+    options = ["rw"];
   };
 }
