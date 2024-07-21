a55bf18664b0e51c22ff880f18456379ab6e014f
MathisWellmann <wellmannmathis@gmail.com>
2024-07-21 12:52:37 +0200

    genoa: update monitors

--- home/genoa.nix
+++ home/genoa.nix
@@ -7,9 +7,9 @@
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [
-        "DP-4, 3840x2160@60, 0x0, 1, transform, 1"
-        "DP-3, 3840x2160@60, 2160x0, 1, transform, 1"
-        "DP-2, 3840x2160@60, 4320x0, 1, transform, 1"
+        "DP-3, 3840x2160@60, 0x0, 1, transform, 1"
+        "DP-2, 3840x2160@60, 2160x0, 1, transform, 1"
+        "DP-1, 3840x2160@60, 4320x0, 1, transform, 1"
       ];
     };
   };
