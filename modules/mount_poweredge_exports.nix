fd2093c6e3b6f1dd223dd6456d12baa256ac58de
MathisWellmann <wellmannmathis@gmail.com>
2024-08-03 22:35:46 +0200

    nfs: mount with `nofail`

--- modules/mount_poweredge_exports.nix
+++ modules/mount_poweredge_exports.nix
@@ -2,16 +2,26 @@
   fileSystems."/mnt/poweredge_video" = {
     device = "poweredge:/SATA_SSD_POOL/video";
     fsType = "nfs";
-    options = ["rw"];
+    options = ["rw" "nofail"];
+  };
+  fileSystems."/mnt/poweredge_series" = {
+    device = "poweredge:/SATA_SSD_POOL/series";
+    fsType = "nfs";
+    options = ["rw" "nofail"];
+  };
+  fileSystems."/mnt/poweredge_movies" = {
+    device = "poweredge:/SATA_SSD_POOL/movies";
+    fsType = "nfs";
+    options = ["rw" "nofail"];
   };
   fileSystems."/mnt/poweredge_music" = {
     device = "poweredge:/SATA_SSD_POOL/music";
     fsType = "nfs";
-    options = ["rw"];
+    options = ["rw" "nofail"];
   };
-  fileSystems."/mnt/poweredge_enc" = {
-    device = "poweredge:/SATA_SSD_POOL/enc";
-    fsType = "nfs";
-    options = ["rw"];
-  };
+  # fileSystems."/mnt/poweredge_enc" = {
+  #   device = "poweredge:/SATA_SSD_POOL/enc";
+  #   fsType = "nfs";
+  #   options = ["rw" "nofail"];
+  # };
 }
