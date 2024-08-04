c7d007d4e0c6cc18f31438fb2d9f38a76489095c
MathisWellmann <wellmannmathis@gmail.com>
2024-08-04 15:57:52 +0200

    external_drives: add kingston usb drive

--- modules/mount_external_drives.nix
+++ modules/mount_external_drives.nix
@@ -3,6 +3,8 @@
   survivor_uuid = "9DD2-3C7B";
   ventoy_mount = "/mnt/ventoy";
   ventoy_uuid = "CA18-AC63";
+  kingston_mount = "/mnt/kingston";
+  kingston_uuid = "9c7d2ce1-0f96-445e-8645-b61e68894659";
 in {
   fileSystems."${survivor_mount}" = {
     device = "/dev/disk/by-uuid/${survivor_uuid}";
@@ -16,6 +18,15 @@ in {
   fileSystems.${ventoy_mount} = {
     device = "/dev/disk/by-uuid/${ventoy_uuid}";
     fsType = "exfat";
+    options = [
+      # If you don't have this options attribute, it'll default to "defaults"
+      "users" # Allows any user to mount and unmount
+      "nofail" # Prevent system from failing if this drive doesn't mount
+    ];
+  };
+  fileSystems.${kingston_mount} = {
+    device = "/dev/disk/by-uuid/${kingston_uuid}";
+    fsType = "ext4";
     options = [
       # If you don't have this options attribute, it'll default to "defaults"
       "users" # Allows any user to mount and unmount
