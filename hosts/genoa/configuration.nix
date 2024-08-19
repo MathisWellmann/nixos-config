554f7050012b54803b4fdeae90f4ad14b480e105
MathisWellmann <wellmannmathis@gmail.com>
2024-08-19 12:34:26 +0200

    genoa: use new module `backup_home_to_remote`

--- hosts/genoa/configuration.nix
+++ hosts/genoa/configuration.nix
@@ -1,15 +1,8 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{
-  pkgs,
-  inputs,
-  ...
-}: let
+{inputs, ...}: let
   username = "magewe";
-  backup_host_ip = "169.254.80.160"; # Using the mellanox 100G NIC
-  backup_host_name = "poweredge";
-  backup_target_dir = "/SATA_SSD_POOL/backup_genoa";
 in {
   imports = [
     # Include the results of the hardware scan.
@@ -25,13 +18,13 @@ in {
     ./../../modules/mount_external_drives.nix
     ./../../modules/mount_poweredge_exports.nix
     ./../../modules/prometheus_exporter.nix
+    ./../../modules/backup_home_to_remote.nix
   ];
 
   networking = {
     hostName = "genoa";
     firewall.allowedTCPPorts = [
       8231 # Tikr
-      2049 # NFS
     ];
   };
 
@@ -67,31 +60,11 @@ in {
     NIXOS_OZONE_WL = 1;
   };
 
-  ### Backup Section ###
-  fileSystems."/mnt/${backup_host_name}_backup" = {
-    device = "${backup_host_ip}:${backup_target_dir}";
-    fsType = "nfs";
-    options = ["rw" "nofail"];
+  services.backup_home_to_remote = {
+    enable = true;
+    local_username = "${username}";
+    backup_host_addr = "169.254.90.239/16";
+    backup_host_name = "poweredge";
+    backup_host_dir = "/SATA_SSD_POOL/backup_genoa";
   };
-  services = {
-    restic.backups = {
-      home = {
-        initialize = true;
-        paths = [
-          "/home/${username}/"
-        ];
-        exclude = [
-          "/home/${username}/.cache/"
-        ];
-        passwordFile = "/etc/nixos/secrets/restic/password";
-        repository = "/mnt/${backup_host_name}_backup/";
-        pruneOpts = ["--keep-daily 14"];
-        user = "${username}";
-      };
-    };
-  };
-  environment.systemPackages = with pkgs; [
-    restic
-    flood
-  ];
 }
