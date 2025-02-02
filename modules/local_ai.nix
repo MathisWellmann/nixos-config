fc2eb41026cb89040d645b56959c7cbc441c7435
MathisWellmann <wellmannmathis@gmail.com>
2025-02-02 13:06:20 +0100

    local_ai: set ollama environment variables to make deepseek-r1:671b work

--- modules/local_ai.nix
+++ modules/local_ai.nix
@@ -1,6 +1,13 @@
 {...}: {
   services.ollama = {
     enable = true;
-    acceleration = "cuda";
+    # acceleration = "null";
+    environmentVariables = {
+      OLLAMA_NUM_PARALLEL="1";
+      OLLAMA_KEEP_ALIVE="1";
+      OLLAMA_SCHED_SPREAD="1";
+      OLLAMA_GPU_OVERHEAD="22000000";
+      OLLAMA_LOAD_TIMEOUT="15m";
+    };
   };
 }
