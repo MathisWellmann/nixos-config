416cf336547b104757f805c9e1c8b0419ae48346
MathisWellmann <wellmannmathis@gmail.com>
2025-03-05 10:26:53 +0100

    split `monero.nix` module into itself and `monero_miner.nix`

--- modules/monero.nix
+++ modules/monero.nix
@@ -12,25 +12,4 @@
     extraConfig = "confirm-external-bind=true";
   };
   networking.firewall.allowedTCPPorts = [18081];
-
-  services.xmrig = {
-    enable = true;
-    settings = {
-      autosave = true;
-      cpu = {
-        enable = true;
-        max-threads-hint = 30;
-      };
-      opencl = false;
-      cuda = false;
-      pools = [
-        {
-          url = "pool.supportxmr.com:443";
-          user = "86FxMgMKate8Vm9Kka41KogveSwYgocQEaiUPgyLq6itBiwh1kch2Y8K5kup91uiTBVKmh8HW4B61U5x5o3hDbMEUrGm5kx";
-          keepalive = true;
-          tls = true;
-        }
-      ];
-    };
-  };
 }
---
+++ modules/monero_miner.nix
@@ -0,0 +1,23 @@
+
+{...}: {
+  services.xmrig = {
+    enable = true;
+    settings = {
+      autosave = true;
+      cpu = {
+        enable = true;
+        max-threads-hint = 50;
+      };
+      opencl = false;
+      cuda = false;
+      pools = [
+        {
+          url = "pool.supportxmr.com:443";
+          user = "86FxMgMKate8Vm9Kka41KogveSwYgocQEaiUPgyLq6itBiwh1kch2Y8K5kup91uiTBVKmh8HW4B61U5x5o3hDbMEUrGm5kx";
+          keepalive = true;
+          tls = true;
+        }
+      ];
+    };
+  };
+}
