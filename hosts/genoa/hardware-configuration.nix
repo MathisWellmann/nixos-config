af1fa7e69d6619f1deaeebe7cdeb14b8f5ad26e7
MathisWellmann <wellmannmathis@gmail.com>
2025-03-26 15:58:40 +0100

    genoa: Add module `static_ips.nix` and set static ip for genoa.

--- hosts/genoa/hardware-configuration.nix
+++ hosts/genoa/hardware-configuration.nix
@@ -6,7 +6,10 @@
   lib,
   modulesPath,
   ...
-}: {
+}:
+let
+  static_ips = import ../../modules/static_ips.nix;
+in {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
@@ -34,42 +37,67 @@
 
   swapDevices = [];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp199s0f4u1u2.useDHCP = lib.mkDefault true;
-  networking.networkmanager.enable = false;
-  networking.interfaces.enp1s0f0np0 = {
-    name = "mellanox-100G-0";
-    useDHCP = false;
-    mtu = 9000;
-    ipv4 = {
-      addresses = [
-        {
-          address = "169.254.3.1";
-          prefixLength = 16;
-        }
-      ];
+  networking = {
+    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+    # (the default) this is the recommended approach. When using systemd-networkd it's
+    # still possible to use this option, but it's recommended to use it in conjunction
+    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+    useDHCP = lib.mkDefault false;
+
+    networkmanager.enable = false;
+
+    defaultGateway = {
+      interface = "eno1";
+      address = "192.168.0.55";
+    };
+    nameservers = [
+      "192.168.0.55"
+      "1.1.1.1"
+      "8.8.8.8"
+      "9.9.9.9"
+    ];
+
+    interfaces = {
+      eno1 = {
+        name = "eno1";
+        useDHCP = false;
+        ipv4 = {
+          addresses = [
+            {
+              address = static_ips.genoa_ip;
+              prefixLength = 24;
+            }
+          ];
+        };
+      };
+      enp1s0f0np0 = {
+        name = "mellanox-100G-0";
+        useDHCP = false;
+        mtu = 9000;
+        ipv4 = {
+          addresses = [
+            {
+              address = "169.254.3.1";
+              prefixLength = 16;
+            }
+          ];
+        };
+      };
+      enp1s0f1np1 = {
+        name = "mellanox-100G-1";
+        useDHCP = false;
+        mtu = 9000;
+        ipv4 = {
+          addresses = [
+            {
+              address = "169.254.3.2";
+              prefixLength = 16;
+            }
+          ];
+        };
+      };
     };
   };
-  networking.interfaces.enp1s0f1np1 = {
-    name = "mellanox-100G-1";
-    useDHCP = false;
-    mtu = 9000;
-    ipv4 = {
-      addresses = [
-        {
-          address = "169.254.3.2";
-          prefixLength = 16;
-        }
-      ];
-    };
-  };
-  # networking.interfaces.eth0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.tailscale0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
