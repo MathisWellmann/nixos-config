0538c4ac1fcee86e3136260383d00cbebd7a49e3
MathisWellmann <wellmannmathis@gmail.com>
2025-04-07 17:52:37 +0200

    poweredge: set `mafl` port to 80

--- hosts/poweredge/configuration.nix
+++ hosts/poweredge/configuration.nix
@@ -11,13 +11,13 @@
   username = "magewe";
   backup_host = "elitedesk";
   backup_target_dir = "/mnt/backup_hdd";
+  mafl_port = 80;
   tikr_base_port = 9184;
   mongodb_port = 27017;
   jellyfin_port = 8096;
   gitea_port = 3000;
   gitea_state_dir = "/var/lib/gitea";
   grafana_port = 3001;
-  mafl_port = 3002;
   homer_port = 3003;
   readeck_port = 3004;
   mealie_port = 3005;
