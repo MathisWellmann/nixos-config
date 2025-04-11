ce1d1c1530512ad619c1ca4f55d2639fc7e0bd11
MathisWellmann <wellmannmathis@gmail.com>
2025-04-11 14:04:44 +0200

    add `harmonia` cache

--- modules/nix_binary_cache_client.nix
+++ modules/nix_binary_cache_client.nix
@@ -3,10 +3,13 @@ let
     poweredge_const = import ../hosts/poweredge/constants.nix;
 in {
     nix.settings.substituters = [
-        # TODO: use port from definition file.
+        # TODO: use port from constants file.
+        "http://${poweredge_const.hostname}:5000"
         "http://${poweredge_const.hostname}:${builtins.toString poweredge_const.ncps_port}"
     ];
     nix.settings.trusted-public-keys = [
+        "${poweredge_const.hostname}:yo2DcCCDywV6ImfqBa3etuuqaYOYmJPX4xzHdRaBmpU="
+        "nixcache.mwtradingsystems.com:yo2DcCCDywV6ImfqBa3etuuqaYOYmJPX4xzHdRaBmpU="
         # Get public key: `curl -X GET http://poweredge:3501`
         "${poweredge_const.hostname}:/HB255KnBl2OThyoTq40lY0W1OcJaApUq6keekmqudc="
     ];
