047cf44f5646cdc0af1e86e2a77c04cff63495a7
MathisWellmann <wellmannmathis@gmail.com>
2025-07-02 22:08:02 +0200

    meshify: remove unused things

--- home/meshify.nix
+++ home/meshify.nix
@@ -5,19 +5,6 @@
 
   wayland.windowManager.hyprland = {
     settings = {
-      # Top left corner is 0x0 is x and y. increasing y means physically a lower position.
-      monitor = [
-        # Triple Vertical
-        "DP-5, preferred, 0x1080, 1, transform, 1"
-        "DP-4, 3840x2160@159, 2160x1080, 1, transform, 1"
-        "DP-3, 3840x2160@159, 4320x1080, 1, transform, 1"
-        # "HDMI-A-5, 1920x1080@60, 3240x0, 1"
-
-        # Middle monitor horizontal
-        # "DP-3, 3840x2160@144, 0x0, 1, transform, 1"
-        # "DP-2, 3840x2160@144, 2160x0, 1"
-        # "DP-1, 3840x2160@144, 6000x0, 1, transform, 1"
-      ];
       env = [
         "LIBVA_DRIVER_NAME,nvidia"
         "XDG_SESSION_TYPE,wayland"
@@ -25,9 +12,6 @@
         "__GLX_VENDOR_LIBRARY_NAME,nvidia"
       ];
       cursor.no_hardware_cursors = true;
-      # env = [
-      #   "AQ_DRM_DEVICES,/dev/dri/card3" # Use AMD GPU.
-      # ];
     };
   };
   services.hyprpaper = {
