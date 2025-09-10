8af5ce0a51696eccb65603bffef07e24bde0580f
MathisWellmann <wellmannmathis@gmail.com>
2025-09-10 18:16:23 +0100

    home: zed config changes

--- home/home_hyprland.nix
+++ home/home_hyprland.nix
@@ -251,11 +251,15 @@
         };
         vim_mode = false;
         helix_mode = true;
-        ui_font_size = 14;
-        buffer_font_size = 14;
+        ui_font_size = 18;
+        buffer_font_size = 18;
         lsp = {
           rust-analyzer.binary.path = "rust-analyzer";
           pylsp.binary.path = "pylsp";
+        };
+        diagnostics.inline = {
+          enabled = true;
+          max_severity = null;
         };
       };
     };
