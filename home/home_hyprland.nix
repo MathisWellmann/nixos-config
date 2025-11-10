47b99f923841166cb3cc0db901402d4f965411a5
MathisWellmann <wellmannmathis@gmail.com>
2025-11-10 09:32:23 -0800

    home_hyprland: set `hypridle` and change fonts

--- home/home_hyprland.nix
+++ home/home_hyprland.nix
@@ -333,6 +333,22 @@
         ];
       };
     };
+    hypridle = {
+      enable = true;
+      settings = {
+        listener = [
+          {
+            timeout = 900;
+            on-timeout = "hyprlock";
+          }
+          {
+            timeout = 1200;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+        ];
+      };
+    };
   };
   dconf = {
     settings = {
