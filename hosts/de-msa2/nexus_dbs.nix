15288393c5cec1f9de4a4fb12d052120231ea9e2
MathisWellmann <wellmannmathis@gmail.com>
2025-11-27 18:26:05 +0100

    de-msa2: disable `mongodb`

--- hosts/de-msa2/nexus_dbs.nix
+++ hosts/de-msa2/nexus_dbs.nix
@@ -1,6 +1,5 @@
 _: let
   const = import ./constants.nix;
-  global_const = import ../../global_constants.nix;
 in {
   # Nexus Database
   virtualisation.oci-containers.containers."greptimedb" = let
@@ -36,12 +35,12 @@ in {
     ];
     extraOptions = ["--ulimit" "memlock=-1"];
   };
-  services.mongodb = {
-    enable = true;
-    dbpath = "/nvme_pool/mongodb";
-    user = "${global_const.username}";
-    bind_ip = "0.0.0.0";
-  };
+  # services.mongodb = {
+  #   enable = true;
+  #   dbpath = "/nvme_pool/mongodb";
+  #   user = "${global_const.username}";
+  #   bind_ip = "0.0.0.0";
+  # };
   # Raise open file limits for mongodb.
   security.pam.services.mongodb.limits = [
     {
