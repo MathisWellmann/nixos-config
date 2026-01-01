143a1da1ab5053be955fc1e1f7f6b366b172761c
MathisWellmann <wellmannmathis@gmail.com>
2026-01-01 22:40:20 +0000

    scripts: properly parse the `macAddress` for each host

--- scripts/wake_on_lan.nix
+++ scripts/wake_on_lan.nix
@@ -5,6 +5,7 @@
 }:
 with builtins; let
   systems = attrNames self.nixosConfigurations;
+  systems_string = concatStringsSep " " systems;
 
   # Get the primary interface of a host as specified by the `defaultGateway`, if any.
   getPrimaryInterface = host: self
@@ -14,41 +15,57 @@ with builtins; let
     .networking
     .defaultGateway
     .interface or "unknown";
+
   # Get the MAC address of a host interface, if any.
   getMacAddress = host:
-    self
+  let
+    ifs = self
       .nixosConfigurations
       .${host}
       .config
       .networking
-      .interfaces
-      .${getPrimaryInterface host}
-      .macAddress or "fallback";
+      .interfaces;
+    prim = getPrimaryInterface host;
+    macAddress = if hasAttr prim ifs
+      then (
+        if (ifs.${prim}.macAddress != null)
+        then ifs.${prim}.macAddress
+        else "no-mac"
+      )
+      else "invalid-interface";
+  in 
+    macAddress;
+
   # Get the mac address of each systems primary NIC.
-  macs = map (host: "${host}@${toString (getMacAddress host)}") systems;
-  systems_string = concatStringsSep " " macs;
+  macs = map (host: "${toString (getMacAddress host)}") systems;
+  macs_string = concatStringsSep " " macs;
 in
   pkgs.writeShellScriptBin "wake_on_lan" ''
+    echo "systems: ${systems_string}"
+    echo "macs: ${macs_string}"
+
     IFS=' ' read -a arr_systems <<< "${systems_string}"
-    echo "systems: ''${arr_systems[@]}"
+    IFS=' ' read -a arr_macs <<< "${macs_string}"
 
     echo "Checking host reachability."
-    items=()
+    offline_systems=()
     for i in ''${!arr_systems[@]}; do
       system="''${arr_systems[$i]}"
+      mac="''${arr_macs[$i]}"
+      echo "Pinging $system @ $mac"
+
       # Try to ping the system once with 1-second timeout
       ping_output=$(ping -c 1 -w 1 "$system" 2>/dev/null)
       if [ $? -eq 0 ]; then
         # Extract latency in ms from ping output
         latency=$(echo "$ping_output" | grep -oP "time=\K[\d.]+" )
+        echo "------> $system is reachable in $latency ms"
       else
-        latency="N/A"
+        echo "$system is not reachable"
+        offline_systems+=("$system=$i")
       fi
-      item="$system-$latency(ms)=$i"
-      echo "$item"
-      items+=($item)
     done
-    CHOICE=$(${pkgs.gum}/bin/gum choose --label-delimiter '=' --header "Select Host to send WakeOnLan magic packet to:" ''${items[@]})
+    CHOICE=$(${pkgs.gum}/bin/gum choose --label-delimiter '=' --header "Select Host to send WakeOnLan magic packet to:" ''${offline_systems[@]})
 
     if [ -z "$CHOICE" ]; then
       echo "No host selected"
@@ -56,10 +73,9 @@ in
     fi
 
     selected_host=''${arr_systems[$CHOICE]}
+    mac=''${arr_macs[$CHOICE]}
 
-    echo "Sending magic WakeOnLan packet to $selected_host"
-
-    # TODO: get MAC address of host from config.
+    echo "Sending magic WakeOnLan packet to $selected_host @ $mac"
 
     # TODO: send magic packet
     # exec ${pkgs.wakeonlan}/bin/wakeonlan $host_mac
