a6f9dcdee20ac095d6241d8d71273f6b38871510
MathisWellmann <wellmannmathis@gmail.com>
2026-01-06 14:52:37 +0000

    Introduce `vms/waterfox.nix` for lightweight QEMU and KVM virtual machines

---
+++ vms/waterfox.nix
@@ -0,0 +1,22 @@
+# Build and run with `nix-build vms/waterfox.nix; result/bin/run-nixos-vm`
+let
+  pkgs = import <nixpkgs> { };
+
+  waterfox_vm = {...}: {
+    users.extraUsers.root.password = "test";
+
+    services.xserver = {
+      enable = true;
+      desktopManager.lxqt.enable = true;
+      displayManager.lightdm.enable = true;
+      displayManager.defaultSession = "lxqt";
+    };
+    environment.systemPackages = with pkgs; [
+      firefox
+    ];
+  };
+  vms = pkgs.nixos[
+    waterfox_vm
+  ];
+in 
+  vms.config.system.build.vm
