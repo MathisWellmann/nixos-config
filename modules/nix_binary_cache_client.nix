4b69191e87529433200a345add68af74e46b4cac
MathisWellmann <wellmannmathis@gmail.com>
2026-03-02 21:03:59 +0100

    improve `nix_binary_cache_client` module

--- modules/nix_binary_cache_client.nix
+++ modules/nix_binary_cache_client.nix
@@ -1,14 +1,12 @@
-_: let
-  poweredge_const = import ../hosts/poweredge/constants.nix;
-in {
-  nix.settings.trusted-substituters = [
-    "http://${poweredge_const.hostname}:${builtins.toString poweredge_const.harmonia_port}"
-    "http://${poweredge_const.hostname}:${builtins.toString poweredge_const.ncps_port}"
-  ];
-  nix.settings.trusted-public-keys = [
-    # Harmonia
-    "poweredge:S7D5KElVnn7cZrQGpoL3Z9X7XRtmk4K9qFcrNoyeoUI="
-    # ncps Get public key: `curl -X GET http://poweredge:3501`
-    "${poweredge_const.hostname}:/HB255KnBl2OThyoTq40lY0W1OcJaApUq6keekmqudc="
-  ];
+_: {
+  nix.settings = {
+    trusted-substituters = [
+      # Harmonia runs on port 5000
+      "http://de-msa2:5000"
+    ];
+    trusted-public-keys = [
+      # Harmonia
+      "de-msa2:pRornjWlGkufROI/KEK82Y3Okmwkc2cJ+LoJ9kaxtqg="
+    ];
+  };
 }
