a65519997e17c56fd92af37e4fb05ac1a419a478
MathisWellmann <wellmannmathis@gmail.com>
2026-03-03 19:03:54 +0100

    nix_binary_cache_client: add two more trusted substituters.

--- modules/nix_binary_cache_client.nix
+++ modules/nix_binary_cache_client.nix
@@ -3,10 +3,14 @@ _: {
     trusted-substituters = [
       # Harmonia runs on port 5000
       "http://de-msa2:5000"
+      "https://nix-community.cachix.org"
+      "https://cache.nixos-cuda.org"
     ];
     trusted-public-keys = [
       # Harmonia
       "de-msa2:pRornjWlGkufROI/KEK82Y3Okmwkc2cJ+LoJ9kaxtqg="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
     ];
   };
 }
