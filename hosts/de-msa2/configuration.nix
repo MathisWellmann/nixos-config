e1f4b3814a01db3d6aea679f9504b676cb901346
MathisWellmann <wellmannmathis@gmail.com>
2026-03-23 19:59:48 +0100

    de-msa2: improve startup message

--- hosts/de-msa2/configuration.nix
+++ hosts/de-msa2/configuration.nix
@@ -109,12 +109,12 @@ in {
         bitmagnet = "bitmagnet";
         greptime_db = "podman-greptimedb";
         dragonfly_db = "podman-dragonfly";
-        # tikr_BinanceCoinMargin_L2OrderBookDelta = "tikr@BinanceCoinMargin_L2OrderBookDelta";
+        tikr_BinanceCoinSpot_Quotes = "tikr@BinanceSpot_Quotes";
+        tikr_BinanceCoinSpot_Trade = "tikr@BinanceSpot_Trade";
         tikr_BinanceCoinMargin_Quotes = "tikr@BinanceCoinMargin_Quotes";
-        tikr_BinanceCoinMargin_Trades = "tikr@BinanceCoinMargin_Trades";
-        # tikr_BinanceUsdMargin_L2OrderBookDelta = "tikr@BinanceUsdMargin_L2OrderBookDelta";
+        tikr_BinanceCoinMargin_Trade = "tikr@BinanceCoinMargin_Trade";
         tikr_BinanceUsdMargin_Quotes = "tikr@BinanceUsdMargin_Quotes";
-        tikr_BinanceUsdMargin_Trades = "tikr@BinanceUsdMargin_Trades";
+        tikr_BinanceUsdMargin_Trade = "tikr@BinanceUsdMargin_Trade";
         github-runner-lfest-rs = "github-runner-de-msa2-lfest-rs";
         github-runner-trade_aggregation-rs = "github-runner-de-msa2-trade_aggregation-rs";
         github-runner-openresponses-rs = "github-runner-de-msa2-openresponses-rs";
