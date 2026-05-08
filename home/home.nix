5059377b6e45f43fd9da3f20ebdbf32033b46dbb
MathisWellmann <wellmannmathis@gmail.com>
2026-05-08 10:42:21 +0100

    home: include `awww` again.

--- home/home.nix
+++ home/home.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: let
+{inputs, pkgs, ...}: let
   const = import ../global_constants.nix;
 in {
   imports = [
@@ -31,7 +31,7 @@ in {
   home.packages = with pkgs; [
     # From inputs
     # inputs.nox.packages.${pkgs.stdenv.hostPlatform.system}.default # Nix options search
-    # inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww
+    inputs.awww.packages.${pkgs.stdenv.hostPlatform.system}.awww
 
     # Fonts
     terminus_font
