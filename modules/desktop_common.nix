f37404d70d8405c39100a69e6548aa403c376dfd
MathisWellmann <wellmannmathis@gmail.com>
2026-05-29 22:19:51 +0200

    desktop: fix audio with FIIO M15S

--- modules/desktop_common.nix
+++ modules/desktop_common.nix
@@ -11,13 +11,47 @@
   # Some tricks:
   # systemctl --user restart pipewire.service
   # systemctl --user restart pipewire-pulse.service
-  nixpkgs.config.pulseaudio = true;
   services = {
     pipewire = {
       enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+      wireplumber.extraConfig = {
+        "99-fiio-m15s" = {
+          "monitor.alsa.rules" = [
+            {
+              matches = [
+                { "device.name" = "~alsa_card\\.usb-FiiO_FiiO_M15S.*"; }
+              ];
+              actions.update-props = {
+                "api.acp.auto-profile" = true;
+                "api.acp.auto-port" = true;
+              };
+            }
+          ];
+
+          # Prefer the normal stereo sink over "off"/"pro-audio" for the FiiO M15S.
+          # The M15S exposes:
+          #   1: output:analog-stereo
+          #   2: output:iec958-stereo
+          #   3: pro-audio
+          "device.profile.priority.rules" = [
+            {
+              matches = [
+                { "device.name" = "~alsa_card\\.usb-FiiO_FiiO_M15S.*"; }
+              ];
+              actions.update-props = {
+                priorities = [
+                  "output:analog-stereo"
+                  "output:iec958-stereo"
+                  "pro-audio"
+                ];
+              };
+            }
+          ];
+        };
+      };
     };
     xserver = {
       enable = true;
