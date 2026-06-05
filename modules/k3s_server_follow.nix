757396774aab0b5a533d022eb909c71fb3d58a44
MathisWellmann <wellmannmathis@gmail.com>
2026-06-05 16:36:27 +0200

    k3s_server_follow: use ip address directly, not the hostname (resolved over tailscale)

--- modules/k3s_server_follow.nix
+++ modules/k3s_server_follow.nix
@@ -1,10 +1,12 @@
-{config, ...}: {
+{config, ...}: let
+  ips = import ./static_ips.nix;
+in {
   age.secrets.k3s_token.file = ../secrets/k3s_token.age;
   services.k3s = {
     enable = true;
     role = "server";
     tokenFile = config.age.secrets.k3s_token.path;
-    serverAddr = "https://de-msa2:6443";
+    serverAddr = "https://${ips.de-msa2_ip}:6443";
   };
   networking.firewall.allowedTCPPorts = [
     2379 # HA
