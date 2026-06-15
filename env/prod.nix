4a350b0ad7b4c8190eec0f65410301368568e5c8
MathisWellmann <wellmannmathis@gmail.com>
2026-06-15 17:41:30 +0100

    env/prod: remove nginx test application.

--- env/prod.nix
+++ env/prod.nix
@@ -16,44 +16,4 @@
     prune = true;
     selfHeal = true;
   };
-
-  applications.nginx = {
-    namespace = "nginx";
-    createNamespace = true;
-
-    resources = {
-      # Deployment with ConfigMap volume
-      deployments.nginx.spec = {
-        replicas = 2;
-        selector.matchLabels.app = "nginx";
-        template = {
-          metadata.labels.app = "nginx";
-          spec = {
-            containers.nginx = {
-              image = "nginx:1.25.1";
-              ports.http.containerPort = 80;
-              volumeMounts."/usr/share/nginx/html".name = "html";
-            };
-            volumes.html.configMap.name = "nginx-html";
-          };
-        };
-      };
-
-      # Service
-      services.nginx.spec = {
-        selector.app = "nginx";
-        ports.http.port = 80;
-      };
-
-      # ConfigMap with HTML content
-      configMaps.nginx-html.data."index.html" = ''
-        <!DOCTYPE html>
-        <html>
-          <body>
-            <h1>Hello from nixidy!</h1>
-          </body>
-        </html>
-      '';
-    };
-  };
 }
