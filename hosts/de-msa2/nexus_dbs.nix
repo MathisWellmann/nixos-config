9353440e1400266b732d6ce3176d8d15810356c8
MathisWellmann <wellmannmathis@gmail.com>
2026-06-19 16:24:49 +0200

    de-msa2: prune old `nexus_dbs.nix` services that now live in the cluster.

--- hosts/de-msa2/nexus_dbs.nix
+++ hosts/de-msa2/nexus_dbs.nix
@@ -1,75 +1,12 @@
 # Rename to `nexus_infra.nix`
 {inputs, ...}: let
   const = import ./constants.nix {};
-  # iggyConfigFile = pkgs.writeText "iggy-server.toml" ''
-  #   [system]
-  #   path = "/nvme_pool/iggy"
-  #   [system.memory_pool]
-  #   enabled = true
-  #   size = "1 GiB"          # default is 4 GiB — reduce this first
-  #   bucket_capacity = 1024  # default 8192 — lower = fewer pre-allocated buffers
-  #   [system.partition]
-  #   # Flush to disk sooner — reduces in-memory message buffer
-  #   messages_required_to_save = 256        # default 1024
-  #   size_of_messages_required_to_save = "256 KiB"  # default 1 MiB
-  #   [system.segment]
-  #   cache_indexes = "none"  # options: "all", "open_segment", "none"
-  #   [system.topic]
-  #   # Add a size cap and/or message expiry if you don't need unbounded retention
-  #   max_size = "10 GiB"             # default "unlimited"
-  #   message_expiry = "2 days"       # default "none"
-  #   [message_saver]
-  #   enabled = true
-  #   enforce_fsync = true
-  #   interval = "30 s"
-  #   [http]
-  #   enabled = true
-  #   address = "0.0.0.0:${toString const.iggy_http_port}"
-  #   web_ui = true
-  #   [tcp]
-  #   enabled = true
-  #   address = "0.0.0.0:${toString const.iggy_tcp_port}"
-  #   [quic]
-  #   enabled = false
-  #   address = "0.0.0.0:${toString const.iggy_quic_port}"
-  #   [websocket]
-  #   enabled = false
-  #   address = "0.0.0.0:${toString const.iggy_websocket_port}"
-  #   [telemetry]
-  #   enabled = false
-  # '';
 in {
   imports = [
     inputs.iggy.nixosModules.default
   ];
 
-  # Nexus Database
-  virtualisation.oci-containers.containers."greptimedb" = let
-    version = "v1.0.0-rc.2";
-  in {
-    image = "greptime/greptimedb:${version}";
-    cmd = [
-      "standalone"
-      "start"
-      "--http-addr"
-      "0.0.0.0:${toString const.greptimedb_http_port}"
-      "--rpc-addr"
-      "0.0.0.0:${toString const.greptimedb_rpc_port}"
-      "--mysql-addr"
-      "0.0.0.0:${toString const.greptimedb_mysql_port}"
-      "--postgres-addr"
-      "0.0.0.0:${toString const.greptimedb_postgres_port}"
-    ];
-    ports = [
-      "${toString const.greptimedb_http_port}:${toString const.greptimedb_http_port}"
-      "${toString const.greptimedb_rpc_port}:${toString const.greptimedb_rpc_port}"
-      "${toString const.greptimedb_mysql_port}:${toString const.greptimedb_mysql_port}"
-      "${toString const.greptimedb_postgres_port}:${toString const.greptimedb_postgres_port}"
-    ];
-    volumes = [
-      "/nvme_pool/greptimedb:/greptimedb_data"
-    ];
-  };
+  # TODO: bring into kubernetes cluster deployment in nexus.
   virtualisation.oci-containers.containers."dragonfly" = {
     image = "docker.dragonflydb.io/dragonflydb/dragonfly";
     ports = [
@@ -77,54 +14,7 @@ in {
     ];
     extraOptions = ["--ulimit" "memlock=-1"];
   };
-  # services.mongodb = {
-  #   enable = true;
-  #   dbpath = "/nvme_pool/mongodb";
-  #   user = "${global_const.username}";
-  #   bind_ip = "0.0.0.0";
-  # };
-  # # Raise open file limits for mongodb.
-  # security.pam.services.mongodb.limits = [
-  #   {
-  #     domain = "*";
-  #     type = "soft";
-  #     item = "nofile";
-  #     value = "1000000";
-  #   }
-  # ];
   networking.firewall.allowedTCPPorts = [
-    const.greptimedb_http_port
-    const.greptimedb_rpc_port
-    const.greptimedb_mysql_port
-    const.greptimedb_postgres_port
-    const.mongodb_port
     const.dragonfly_port
   ];
-
-  services = {
-    # From module `inputs.nexus.nixosModules.tikr-iggy`
-    # tikr-iggy = {
-    #   enable = true;
-    #   iggy-addr = "127.0.0.1:${toString const.iggy_tcp_port}";
-    #   exchanges = [
-    #     "BinanceUsdMargin"
-    #     "BinanceCoinMargin"
-    #     "BinanceSpot"
-    #   ];
-    #   data-types = [
-    #     "Trade"
-    #     "Quotes"
-    #   ];
-    #   prometheus_exporter_base_port = const.tikr_base_port;
-    #   environment-file = "/etc/secrets/tikr-iggy";
-    # };
-    # Using the deployment on the k3s cluster now.
-    # iggy-server = {
-    #   enable = true;
-    #   openFirewall = true;
-    #   dataDir = "/nvme_pool/iggy";
-    #   envFile = "/etc/secrets/iggy_server";
-    #   configFile = iggyConfigFile;
-    # };
-  };
 }
