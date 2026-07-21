773dfebd5d8ffda2e8477646e79bab366bc637f9
MathisWellmann <wellmannmathis@gmail.com>
2026-07-21 20:04:15 +0200

    meshify: include `unsloth/gemma-4-31B-it-GGUF` in `localModels`

--- hosts/meshify/constants.nix
+++ hosts/meshify/constants.nix
@@ -3,8 +3,9 @@ _: {
   vllm_port = 8000;
   llama-cpp_port = 8001;
   tensorrt_port = 8002;
-  localModel = "InternScience/Agents-A1-Q8_0-GGUF";
+  localModel = "unsloth/gemma-4-31B-it-GGUF:UD-Q8_K_XL";
   localModels = [
+    "unsloth/gemma-4-31B-it-GGUF:UD-Q8_K_XL"
     "InternScience/Agents-A1-Q8_0-GGUF"
     "deepreinforce-ai/Ornith-1.0-35B-GGUF:Q8_0"
   ];
