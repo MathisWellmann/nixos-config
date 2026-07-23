49efaf7fe7ad29609b0c294d6b52cd82a140d3a9
MathisWellmann <wellmannmathis@gmail.com>
2026-07-23 21:45:21 +0200

    meshify: minor tweaks

--- hosts/meshify/constants.nix
+++ hosts/meshify/constants.nix
@@ -5,8 +5,9 @@ _: {
   tensorrt_port = 8002;
   localModel = "unsloth/gemma-4-31B-it-GGUF:UD-Q8_K_XL";
   localModels = [
-    "unsloth/gemma-4-31B-it-GGUF:UD-Q8_K_XL"
-    "InternScience/Agents-A1-Q8_0-GGUF"
+    "unsloth/gemma-4-31B-it-GGUF:UD-Q8_K_XL" # Slow at ~30TPS
+    "InternScience/Agents-A1-Q8_0-GGUF" # Fast at ~120TPS
     "deepreinforce-ai/Ornith-1.0-35B-GGUF:Q8_0"
+    "unsloth/Qwen3.6-35B-A3B-MTP-GGUF:UD-Q4_K_XL"
   ];
 }
