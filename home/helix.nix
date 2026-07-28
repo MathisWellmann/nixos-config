258b01cc957959d1d6d92f6562901928d5d6b2f3
MathisWellmann <wellmannmathis@gmail.com>
2026-07-29 00:54:30 +0200

    helix: remove some custom keybindings.

--- home/helix.nix
+++ home/helix.nix
@@ -60,10 +60,6 @@
         "f" = "file_picker";
         "l" = "move_visual_line_up";
         "w" = "move_visual_line_down";
-        "m" = "move_char_left";
-        # "n" = "move_char_right";
-        # "N" = "move_next_word_start";
-        "M" = "move_prev_word_start";
       };
       editor = {
         scroll-lines = 3;
