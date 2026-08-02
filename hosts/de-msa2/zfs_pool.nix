7b68879bc91e2ba356365e5c5734dc22bb074021
MathisWellmann <wellmannmathis@gmail.com>
2026-08-02 10:06:47 +0200

    de-msa2: raise ZFS ARC cache to 32GB

--- hosts/de-msa2/zfs_pool.nix
+++ hosts/de-msa2/zfs_pool.nix
@@ -8,7 +8,7 @@ in {
     # sudo sh -c 'echo 17179869184 > /sys/module/zfs/parameters/zfs_arc_max'
     # Then clearing the caches:
     # sudo sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches'
-    kernelParams = ["zfs.zfs_arc_max=16000000000"]; # 16GB ARC size limit
+    kernelParams = ["zfs.zfs_arc_max=32000000000"]; # 32GB ARC size limit
     zfs = {
       forceImportRoot = false;
       extraPools = [
