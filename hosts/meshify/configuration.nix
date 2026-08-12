ae7acb101ecab21057c92587bf0c68c8fc8e1a24
MathisWellmann <wellmannmathis@gmail.com>
2026-08-12 23:13:05 +0200

    meshify: fix small compilation issue

--- hosts/meshify/configuration.nix
+++ hosts/meshify/configuration.nix
@@ -35,7 +35,6 @@ in {
     (import ./../../modules/ai/pi-agent.nix {
       baseUrl = "http://127.0.0.1:${toString const.llama-cpp_port}/v1";
       enableAgentica = true;
-      inherit (const) localModel;
       vllmBaseUrl = "http://127.0.0.1:${toString const.vllm_port}/v1";
       vllmModels = [const.vllmModel];
     })
