2705c51dd7509d16885193e22caef795ae1d9298
MathisWellmann <wellmannmathis@gmail.com>
2026-09-03 15:27:13 +0100

    nix flake update

--- modules/ai/local_ai.nix
+++ modules/ai/local_ai.nix
@@ -7,10 +7,10 @@
     in {
   environment.systemPackages = with pkgs; [
     # inputs.forgecode.packages.${pkgs.stdenv.hostPlatform.system}.forge
-    inputs.maki.packages.${system}.default
+    # inputs.maki.packages.${system}.maki
     inputs.llm-agents.packages.${system}.omp
     inputs.llm-agents.packages.${system}.prime-agent
-    inputs.autolith.packages.${system}.default
+    # inputs.autolith.packages.${system}.default
     # mistral-rs
     claude-code
     codex
