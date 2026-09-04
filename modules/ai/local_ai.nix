ebf29a908cf2eeb56e9a9b37fa249eb18494dcc9
MathisWellmann <wellmannmathis@gmail.com>
2026-09-04 16:57:22 +0100

    re-enable maki

--- modules/ai/local_ai.nix
+++ modules/ai/local_ai.nix
@@ -10,7 +10,7 @@
   ];
   environment.systemPackages = with pkgs; [
     # inputs.forgecode.packages.${pkgs.stdenv.hostPlatform.system}.forge
-    # inputs.maki.packages.${system}.maki
+    inputs.maki.packages.${system}.default
     inputs.llm-agents.packages.${system}.omp
     inputs.llm-agents.packages.${system}.prime-agent
     # inputs.autolith.packages.${system}.default
